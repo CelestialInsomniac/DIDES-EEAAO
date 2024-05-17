378cf1d07ed90437a758877e91fed20666cdfbd7
CelestialInsomniac <annina.pfister@stud.hslu.ch>
2024-05-17 14:05:26 +0200

    Custom Icon hinzugefügt

--- javascript.js
+++ javascript.js
@@ -4,19 +4,31 @@ var map = L.map('map', {
     minZoom: -5
 });
 
+var ring = L.icon({
+    iconUrl: 'images/test_leaflet map V1.0/icons/icon_ring.png',
+    //shadowUrl: 'leaf-shadow.png',
+    iconSize: [120, 120], // size of the icon
+    //shadowSize:   [50, 64], // size of the shadow
+    iconAnchor: [60, 60], // point of the icon which will correspond to marker's location
+    //shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
 var bounds = [[0, 0], [1500, 1500]];
 var image = L.imageOverlay('images/test_leaflet map V1.0/Background_V1.png', bounds).addTo(map);
 
 // Fit the map to the bounds of the image
 map.fitBounds(bounds);
 
-// Set the initial view to center of the image
+// Set the initial view to center of the image, and zoom level 1
 map.setView([750, 750], 1);
 
-// Marker 1
-var point1 = L.latLng([703, 600]);  // Adjust coordinates to be within bounds
-L.marker(point1).addTo(map).bindPopup('This is a test popup - point1').openPopup;
+// Test-Marker
+//var point1 = L.latLng([703, 600]);  // Adjust coordinates to be within bounds
+//L.marker(point1).addTo(map).bindPopup('This is a test popup - point1').openPopup;
 
-// Marker 2
-var point2 = L.latLng([903, 778]);  // Adjust coordinates to be within bounds
-L.marker(point2).addTo(map).bindPopup('This is a test popup - point2').openPopup;
+//Marker 3
+L.marker([910, 780], { icon: ring }).addTo(map).bindPopup('This is a test popup - custompoint1').openPopup;
+
+//Marker 4
+L.marker([703, 600], { icon: ring }).addTo(map).bindPopup('This is a test popup - custompoint2').openPopup;
