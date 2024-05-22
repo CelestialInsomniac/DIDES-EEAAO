a15b3feda39dc26b4ccdc5978597856c3e23c7d1
CelestialInsomniac <anninapfister@hotmail.ch>
2024-05-22 10:35:24 +0200

    Score mit Bagel hinzugefügt, Code aufgeräumt

--- javascript.js
+++ javascript.js
@@ -6,9 +6,9 @@ var map = L.map('map', {
 
 var ring = L.icon({
     iconUrl: 'images/test_leaflet map V1.0/icons/icon_ring.png',
-    iconSize: [120, 120], // size of the icon
-    iconAnchor: [60, 60], // point of the icon which will correspond to marker's location
-    popupAnchor: [-3, -76] // point from which the popup should open relative to the iconAnchor
+    iconSize: [120, 120],
+    iconAnchor: [60, 60],
+    popupAnchor: [-3, -76]
 });
 
 var bounds = [[0, 0], [1500, 1500]];
@@ -17,17 +17,33 @@ var image = L.imageOverlay('images/test_leaflet map V1.0/Background_V1.png', bou
 // Fit the map to the bounds of the image
 map.fitBounds(bounds);
 
-// Set the initial view to center of the image, and zoom level 1
+// Grundausrichtung mittig, Zoom Level 1
 map.setView([750, 750], 1);
 
+// Infomarker
 var customPopupInfo = "<div class='custom-popup'>Wie wird gespielt? \r \n Ziehe die Netzwerkkarte. Wenn du einen Knotenpunkt anklickst, erscheint ein Quizfenster. Errate die richtige Antwort und sammle so viele Bagels wie möglich! \r Wenn du die Seite neu lädst, verlierst du deine Bagels.</div>";
 
-// Infomarker
 L.marker([910, 780], { icon: ring }).addTo(map).bindPopup(customPopupInfo, {
     className: 'custom-popup'
 }).openPopup();
 
-// Function to add a marker with a quiz
+// Score-Element initialisieren
+var scoreElement = document.getElementById('score');
+if (scoreElement.innerText.trim() === '') {
+    var score = 0;
+    scoreElement.innerText = score;
+} else {
+    // Score value extrahieren
+    var score = parseInt(scoreElement.innerText);
+}
+
+// Funktion zur Aktualisierung des Scores
+function updateScore() {
+    score++;
+    scoreElement.innerText = score;
+}
+
+// Funktion Quizmarker hinzufügen
 function addQuizMarker(lat, lng, question, answers, correctAnswerIndex, imageUrl, questionId) {
     var quizContent = `
         <div id="quiz-${questionId}">
@@ -67,15 +83,16 @@ function handleAnswer(selectedAnswerId, isCorrect, correctAnswerIndex, questionI
 
     if (isCorrect) {
         selectedAnswer.style.backgroundColor = '#30D3B6';
+        updateScore(); // Aktualisiere den Score bei richtiger Antwort
     } else {
         selectedAnswer.style.backgroundColor = '#EC872A';
         correctAnswer.style.backgroundColor = '#30D3B6';
     }
 
-    // Store the answer
+    // Antworten speichern
     sessionStorage.setItem('quiz-' + questionId, selectedAnswerId);
 
-    // Deactivate all buttons after an answer
+    // Deaktivieren der Knöpfe nach Antwort
     var buttons = document.querySelectorAll(`#quiz-${questionId} .answer`);
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].disabled = true;
@@ -94,7 +111,7 @@ function displayStoredAnswer(questionId, correctAnswerIndex) {
         correctAnswer.style.backgroundColor = '#30D3B6';
     }
 
-    // Deactivate all buttons
+    // Alle Knöpfe deaktivieren
     var buttons = document.querySelectorAll(`#quiz-${questionId} .answer`);
     for (var i = 0; i < buttons.length; i++) {
         buttons[i].disabled = true;
@@ -104,3 +121,4 @@ function displayStoredAnswer(questionId, correctAnswerIndex) {
 // Quizmarkers
 addQuizMarker(703, 600, 'Zu welchem Game gehört dieser Soundeffekt?', ['Super Smash Bros.', 'Metroid', 'Metal Gear Solid'], 0, 'images/test_leaflet map V1.0/fragen/Frage1.png', 'frage1');
 addQuizMarker(525, 820, 'Auf wen spielt Waymonds Gürteltasche an?', ['Data aus Die Goonies', 'Penny aus Inspector Gadget ', 'Peter aus Ghostbusters'], 0, 'images/test_leaflet map V1.0/fragen/Frage2.png', 'frage2');
+addQuizMarker(390, 1273, 'Aus welchem Film stammt diese Szene ursprünglich?', ['2001: Odyssee im Weltraum', 'Planet der Affen', 'King Kong'], 0, 'images/test_leaflet map V1.0/fragen/Frage3.png', 'frage3');
